f6b26d49b0fcebd5f95fcd2b117858b1248be9fb
ajaxchen <1164542190@qq.com>
2019-04-05 16:48:05 +0800

    feat: ip伪造

---
+++ common/axios.js
@@ -0,0 +1,18 @@
+const axios = require('axios');
+
+
+// 生成随机ip地址
+function r(min, max) {
+  return Math.floor(min + Math.random() * (max - min));
+}
+function getRandomIp() {
+  return `${r(1, 255)}.${r(1, 255)}.${r(1, 255)}.${r(1, 255)}`;
+}
+
+axios.interceptors.request.use((config) => {
+  config.headers.common['X-Forwarded-For'] = getRandomIp();
+  console.log(config);
+  return config;
+});
+
+module.exports = axios;
